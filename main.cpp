39725ce8bfd785a15b2a5adf831a8fe08e1f0a78
DaHoC <dahoc3150@googlemail.com>
2012-12-09 13:55:40 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,303 @@
+/**
+ * @file:   main.cpp
+ * @author: Jan Hendriks (dahoc3150 [at] yahoo.com)
+ * @date:   Created on 2. Dezember 2012
+ * @brief:  Example program on how to train your custom HOG detecting vector
+ * for use with openCV <code>hog.setSVMDetector(_descriptor)</code>;
+ * 
+ * For the paper regarding Histograms of Oriented Gradients (HOG), @see http://lear.inrialpes.fr/pubs/2005/DT05/
+ * You can populate the positive samples dir with files from the INRIA person detection dataset, @see http://pascal.inrialpes.fr/data/human/
+ * This program uses SVMlight as machine learning algorithm (@see http://svmlight.joachims.org/), but is not restricted to it
+ * Tested in Ubuntu Linux 64bit 12.04 "Precise Pangolin" with openCV 2.3.1, SVMlight 6.02, g++ 4.6.3
+ * and standard HOG settings, training images of size 64x128px.
+ * 
+ * What this program basically does:
+ * 1. Read positive and negative training sample image files from specified directories
+ * 2. Calculate their HOG features and keep track of their classes (pos, neg)
+ * 3. Save the feature map (vector of vectors/matrix) to file system
+ * 4. Read in and pass the features and their classes to a machine learning algorithm, e.g. SVMlight
+ * 5. Train the machine learning algorithm using the specified parameters
+ * 6. Use the calculated support vectors and SVM model to calculate a single detecting descriptor vector
+ * 
+ * Build by issuing:
+ * g++ `pkg-config --cflags opencv` -c -g -MMD -MP -MF main.o.d -o main.o main.cpp
+ * gcc -c -g `pkg-config --cflags opencv` -MMD -MP -MF svmlight/svm_learn.o.d -o svmlight/svm_learn.o svmlight/svm_learn.c
+ * gcc -c -g `pkg-config --cflags opencv` -MMD -MP -MF svmlight/svm_hideo.o.d -o svmlight/svm_hideo.o svmlight/svm_hideo.c
+ * gcc -c -g `pkg-config --cflags opencv` -MMD -MP -MF svmlight/svm_common.o.d -o svmlight/svm_common.o svmlight/svm_common.c
+ * g++ `pkg-config --cflags opencv` -o trainhog main.o svmlight/svm_learn.o svmlight/svm_hideo.o svmlight/svm_common.o `pkg-config --libs opencv`
+ * 
+ * Warning:
+ * Be aware that the program may consume a considerable amount of main memory, hard disk memory and time, dependent on the amount of training samples.
+ * Also be aware that (esp. for 32bit systems), there are limitations for the maximum file size which may take effect when writing the features file.
+ * 
+ * Terms of use:
+ * This program is to be used as an example and is provided on an "as-is" basis without any warranties of any kind, either express or implied.
+ * Use at your own risk.
+ * For used third-party software, refer to their respective terms of use and licensing.
+ */
+
+#include <stdio.h>
+#include <dirent.h>
+#include <ios>
+#include <fstream>
+#include <stdexcept>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+
+#include "svmlight/svmlight.h"
+
+using namespace std;
+using namespace cv;
+
+// <editor-fold defaultstate="collapsed" desc="Parameter definitions">
+/* Parameter definitions */
+
+// Directory containing positive sample images
+static string posSamplesDir = "pos/";
+// Directory containing negative sample images
+static string negSamplesDir = "neg/";
+// Set the file to write the features to
+static string featuresFile = "genfiles/features.dat";
+// Set the file to write the SVM model to
+static string svmModelFile = "genfiles/svmlightmodel.dat";
+// Set the file to write the resulting detecting descriptor vector to
+static string descriptorVectorFile = "genfiles/descriptorvector.dat";
+
+// HOG parameters for training that for some reason are not included in the HOG class
+static const Size trainingPadding = Size(0, 0);
+static const Size winStride = Size(8, 8);
+// </editor-fold>
+
+// <editor-fold defaultstate="collapsed" desc="Helper functions">
+/* Helper functions */
+
+static string toLowerCase(const string& in) {
+    string t;
+    for (string::const_iterator i = in.begin(); i != in.end(); ++i) {
+        t += tolower(*i);
+    }
+    return t;
+}
+
+static void storeCursor(void) {
+    printf("\033[s");
+}
+
+static void resetCursor(void) {
+    printf("\033[u");
+}
+
+/**
+ * Saves the given descriptor vector to a file
+ * @param descriptorVector the descriptor vector to save
+ * @param _vectorIndices contains indices for the corresponding vector values (e.g. descriptorVector(0)=3.5f may have index 1)
+ * @param fileName
+ * @TODO Use _vectorIndices to write correct indices
+ */
+static void saveDescriptorVectorToFile(vector<float>& descriptorVector, vector<unsigned int>& _vectorIndices, string fileName) {
+    printf("Saving descriptor vector to file '%s'\n", fileName.c_str());
+    /// @WARNING: This is really important, ROS seems to set the system locale which takes decimal commata instead of points which causes the file input parsing to fail
+    setlocale(LC_ALL, "C"); // Do not use the system locale
+    string separator = " "; // Use blank as default separator between single features
+    fstream File;
+    float percent;
+    File.open(fileName.c_str(), ios::out);
+    if (File.is_open()) {
+        //        File << "# This file contains the trained descriptor vector" << endl;
+        printf("Saving descriptor vector features:\t");
+//            printf("\n#features %d", descriptorVector->size());
+        storeCursor();
+        for (int feature = 0; feature < descriptorVector.size(); ++feature) {
+            if ((feature % 10 == 0) || (feature == (descriptorVector.size()-1)) ) {
+                percent = ((1 + feature) * 100 / descriptorVector.size());
+                printf("%4u (%3.0f%%)", feature, percent);
+                fflush(stdout);
+                resetCursor();
+            }
+            //                File << _vectorIndices->at(feature) << ":";
+            File << descriptorVector.at(feature) << separator;
+        }
+        printf("\n");
+        File << endl;
+        File.flush();
+        File.close();
+    }
+}
+
+/**
+ * For unixoid systems only: Lists all files in a given directory and returns a vector of path+name in string format
+ * @param dirName
+ * @param fileNames found file names in specified directory
+ * @param validExtensions containing the valid file extensions for collection in lower case
+ * @return 
+ */
+static void getFilesInDirectory(const string& dirName, vector<string>& fileNames, const vector<string>& validExtensions) {
+    printf("Opening directory %s\n", dirName.c_str());
+    struct dirent* ep;
+    size_t extensionLocation;
+    DIR* dp = opendir(dirName.c_str());
+    if (dp != NULL) {
+        while ((ep = readdir(dp))) {
+            // Ignore (sub-)directories like . , .. , .svn, etc.
+            if (ep->d_type & DT_DIR) {
+                continue;
+            }
+            extensionLocation = string(ep->d_name).find_last_of("."); // Assume the last point marks beginning of extension like file.ext
+            // Check if extension is matching the wanted ones
+            string tempExt = toLowerCase(string(ep->d_name).substr(extensionLocation + 1));
+            if (find(validExtensions.begin(), validExtensions.end(), tempExt) != validExtensions.end()) {
+                printf("Found matching data file '%s'\n", ep->d_name);
+                fileNames.push_back((string) dirName + ep->d_name);
+            } else {
+                printf("Found file does not match required file type, skipping: '%s'\n", ep->d_name);
+            }
+        }
+        (void) closedir(dp);
+    } else {
+        printf("Error opening directory '%s'!\n", dirName.c_str());
+    }
+    return;
+}
+
+/**
+ * This is the actual calculation from the (input) image data to the HOG descriptor/feature vector using the hog.compute() function
+ * @param imageFilename file path of the image file to read and calculate feature vector from
+ * @param descriptorVector the returned calculated feature vector<float> , 
+ *      I can't comprehend why openCV implementation returns std::vector<float> instead of cv::MatExpr_<float> (e.g. Mat<float>)
+ * @param hog HOGDescriptor containin HOG settings
+ */
+static void calculateFeaturesFromInput(const string& imageFilename, vector<float>& featureVector, HOGDescriptor& hog) {
+    /** for imread flags from openCV documentation, 
+     * @see http://docs.opencv.org/modules/highgui/doc/reading_and_writing_images_and_video.html?highlight=imread#Mat imread(const string& filename, int flags)
+     * @note If you get a compile-time error complaining about following line (esp. imread),
+     * you either do not have a current openCV version (>2.0) 
+     * or the linking order is incorrect, try g++ -o openCVHogTrainer main.cpp `pkg-config --cflags --libs opencv`
+     */
+    Mat imageData = imread(imageFilename, 0);
+    if (imageData.empty()) {
+        featureVector.clear();
+        printf("Error: HOG image '%s' is empty, features calculation skipped!\n", imageFilename.c_str());
+        return;
+    }
+    // Check for mismatching dimensions
+    if (imageData.cols != hog.winSize.width || imageData.rows != hog.winSize.height) {
+        featureVector.clear();
+        printf("Error: Image '%s' dimensions (%u x %u) do not match HOG window size (%u x %u)!\n", imageFilename.c_str(), imageData.cols, imageData.rows, hog.winSize.width, hog.winSize.height);
+        return;
+    }
+    vector<Point> locations;
+    hog.compute(imageData, featureVector, winStride, trainingPadding, locations);
+    imageData.release(); // Release the image again after features are extracted
+}
+// </editor-fold>
+
+/**
+ * Main program entry point
+ * @param argc
+ * @param argv
+ * @return EXIT_SUCCESS (0) or EXIT_FAILURE (1)
+ */
+int main(int argc, char** argv) {
+
+    // <editor-fold defaultstate="collapsed" desc="Init">
+    HOGDescriptor hog; // Use standard parameters here
+    // Get the files to train from somewhere
+    static vector<string> positiveTrainingImages;
+    static vector<string> negativeTrainingImages;
+    static vector<string> validExtensions;
+    validExtensions.push_back("jpg");
+    validExtensions.push_back("png");
+    validExtensions.push_back("ppm");
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Read image files">
+    getFilesInDirectory(posSamplesDir, positiveTrainingImages, validExtensions);
+    getFilesInDirectory(negSamplesDir, negativeTrainingImages, validExtensions);
+    /// Retrieve the descriptor vectors from the samples
+    unsigned long overallSamples = positiveTrainingImages.size() + negativeTrainingImages.size();
+    // </editor-fold>
+    
+    // <editor-fold defaultstate="collapsed" desc="Calculate HOG features and save to file">
+    // Make sure there are actually samples to train
+    if (overallSamples == 0) {
+        printf("No training sample files found, nothing to do!\n");
+        return EXIT_SUCCESS;
+    }
+
+    /// @WARNING: This is really important, some libraries (e.g. ROS) seems to set the system locale which takes decimal commata instead of points which causes the file input parsing to fail
+    setlocale(LC_ALL, "C"); // Do not use the system locale
+    setlocale(LC_NUMERIC,"C");
+    setlocale(LC_ALL, "POSIX");
+
+    printf("Reading files, generating HOG features and save them to file '%s':\n", featuresFile.c_str());
+    float percent;
+    /**
+     * Save the calculated descriptor vectors to a file in a format that can be used by SVMlight for training
+     * @NOTE: If you split these steps into separate steps: 
+     * 1. calculating features into memory (e.g. into a cv::Mat or vector< vector<float> >), 
+     * 2. saving features to file / directly inject from memory to machine learning algorithm,
+     * the program may consume a considerable amount of main memory
+     */ 
+    fstream File;
+    File.open(featuresFile.c_str(), ios::out);
+    if (File.good() && File.is_open()) {
+        File << "# Use this file to train, e.g. SVMlight by issuing $ svm_learn -i 1 -a weights.txt " << featuresFile.c_str() << endl; // Remove this line for libsvm which does not support comments
+        // Iterate over sample images
+        for (unsigned long currentFile = 0; currentFile < overallSamples; ++currentFile) {
+            storeCursor();
+            vector<float> featureVector;
+            // Get positive or negative sample image file path
+            const string currentImageFile = (currentFile < positiveTrainingImages.size() ? positiveTrainingImages.at(currentFile) : negativeTrainingImages.at(currentFile - positiveTrainingImages.size()));
+            // Output progress
+            if ( (currentFile+1) % 10 == 0 || (currentFile+1) == overallSamples ) {
+                percent = ((currentFile+1) * 100 / overallSamples);
+                printf("%5lu (%3.0f%%):\tFile '%s'", (currentFile+1), percent, currentImageFile.c_str());
+                fflush(stdout);
+                resetCursor();
+            }
+            // Calculate feature vector from current image file
+            calculateFeaturesFromInput(currentImageFile, featureVector, hog);
+            if (!featureVector.empty()) {
+                /* Put positive or negative sample class to file, 
+                 * true=positive, false=negative, 
+                 * and convert positive class to +1 and negative class to -1 for SVMlight
+                 */
+                File << ((currentFile < positiveTrainingImages.size()) ? "+1" : "-1");
+                // Save feature vector components
+                for (unsigned int feature = 0; feature < featureVector.size(); ++feature) {
+                    File << " " << (feature + 1) << ":" << featureVector.at(feature);
+                }
+                File << endl;
+            }
+        }
+        printf("\n");
+        File.flush();
+        File.close();
+    } else {
+        printf("Error opening file '%s'!\n", featuresFile.c_str());
+        return EXIT_FAILURE;
+    }
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Pass features to machine learning algorithm">
+    /// Read in and train the calculated feature vectors
+    printf("Calling SVMlight\n");
+    SVMlight::getInstance()->read_problem(const_cast<char*> (featuresFile.c_str()));
+    SVMlight::getInstance()->train(); // Call the core libsvm training procedure
+    printf("Training done, saving model file!\n");
+    SVMlight::getInstance()->saveModelToFile(svmModelFile);
+    // </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="Generate single detecting feature vector from calculated SVM support vectors and SVM model">
+    printf("Generating representative single HOG feature vector using svmlight!\n");
+    vector<float> descriptorVector;
+    vector<unsigned int> descriptorVectorIndices;
+    // Generate a single detecting feature vector (v1 | b) from the trained support vectors, for use e.g. with the HOG algorithm
+    SVMlight::getInstance()->getSingleDetectingVector(descriptorVector, descriptorVectorIndices);
+    // And save the precious to file system
+    saveDescriptorVectorToFile(descriptorVector, descriptorVectorIndices, descriptorVectorFile);
+    // </editor-fold>
+
+    return EXIT_SUCCESS;
+}
---
+++ svmlight/svmlight.h
@@ -0,0 +1,153 @@
+/** 
+ * @file:   svmlight.h
+ * @author: Jan Hendriks (dahoc3150 [at] yahoo.com)
+ * @date:   Created on 11. Mai 2011
+ * @brief:  Wrapper interface for SVMlight, 
+ * @see http://www.cs.cornell.edu/people/tj/svm_light/ for SVMlight details and terms of use
+ * 
+ */
+
+#ifndef SVMLIGHT_H
+#define	SVMLIGHT_H
+
+#include <stdio.h>
+#include <vector>
+// svmlight related
+// namespace required for avoiding collisions of declarations (e.g. LINEAR being declared in flann, svmlight and libsvm)
+namespace svmlight {
+    extern "C" {
+        #include "svm_common.h"
+        #include "svm_learn.h"
+    }
+}
+
+using namespace svmlight;
+
+class SVMlight {
+private:
+    DOC** docs; // training examples
+    long totwords, totdoc, i; // support vector stuff
+    double* target;
+    double* alpha_in;
+    KERNEL_CACHE* kernel_cache;
+    MODEL* model; // SVM model
+
+    SVMlight() {
+        // Init variables
+        alpha_in = NULL;
+        kernel_cache = NULL; // Cache not needed with linear kernel
+        model = (MODEL *) my_malloc(sizeof (MODEL));
+        learn_parm = new LEARN_PARM;
+        kernel_parm = new KERNEL_PARM;
+        // Init parameters
+        verbosity = 1; // Show some messages -v 1
+        learn_parm->alphafile[0] = ' '; // NULL; // Important, otherwise files with strange/invalid names appear in the working directory
+        //        learn_parm->alphafile = NULL; // Important, otherwise files with strange/invalid names appear in the working directory
+        learn_parm->biased_hyperplane = 1;
+        learn_parm->sharedslack = 0; // 1
+        learn_parm->skip_final_opt_check = 0;
+        learn_parm->svm_maxqpsize = 10;
+        learn_parm->svm_newvarsinqp = 0;
+        learn_parm->svm_iter_to_shrink = 2; // 2 is for linear;
+        learn_parm->kernel_cache_size = 40;
+        learn_parm->maxiter = 100000;
+        learn_parm->svm_costratio = 1.0;
+        learn_parm->svm_costratio_unlab = 1.0;
+        learn_parm->svm_unlabbound = 1E-5;
+        learn_parm->eps = 0.1;
+        learn_parm->transduction_posratio = -1.0;
+        learn_parm->epsilon_crit = 0.001;
+        learn_parm->epsilon_a = 1E-15;
+        learn_parm->compute_loo = 0;
+        learn_parm->rho = 1.0;
+        learn_parm->xa_depth = 0;
+        // The HOG paper uses a soft classifier (C = 0.01), set to 0.0 to get the default calculation
+        learn_parm->svm_c = 0.01; // -c 0.01
+        learn_parm->type = REGRESSION;
+        learn_parm->remove_inconsistent = 0; // -i 0 - Important
+        kernel_parm->rbf_gamma = 1.0;
+        kernel_parm->coef_lin = 1;
+        kernel_parm->coef_const = 1;
+        kernel_parm->kernel_type = LINEAR; // -t 0
+        kernel_parm->poly_degree = 3;
+    }
+
+    virtual ~SVMlight() {
+        // Cleanup area
+        // Free the memory used for the cache
+        if (kernel_cache)
+            kernel_cache_cleanup(kernel_cache);
+        free(alpha_in);
+        free_model(model, 0);
+        for (i = 0; i < totdoc; i++)
+            free_example(docs[i], 1);
+        free(docs);
+        free(target);
+    }
+
+public:
+    LEARN_PARM* learn_parm;
+    KERNEL_PARM* kernel_parm;
+
+    static SVMlight* getInstance();
+
+    inline void saveModelToFile(const std::string _modelFileName, const std::string _identifier = "svmlight") {
+        write_model(const_cast<char*>(_modelFileName.c_str()), model);
+    }
+
+    void loadModelFromFile(const std::string _modelFileName, const std::string _identifier = "svmlight") {
+        this->model = read_model(const_cast<char*>(_modelFileName.c_str()));
+    }
+
+    // read in a problem (in svmlight format)
+    void read_problem(char *filename) {
+        // Reads and parses the specified file
+        read_documents(filename, &docs, &target, &totwords, &totdoc);
+    }
+
+    // Calls the actual machine learning algorithm
+    void train() {
+        svm_learn_regression(docs, target, totdoc, totwords, learn_parm, kernel_parm, &kernel_cache, model);
+    }
+
+    /**
+     * Generates a single detecting feature vector (v1 | b) from the trained support vectors, for use e.g. with the HOG algorithm
+     * vec1 = sum_1_n (alpha_y*x_i). (vec1 is a 1 x n column vector. n = feature vector length )
+     * @param singleDetectorVector resulting single detector vector for use in openCV HOG
+     * @param singleDetectorVectorIndices
+     */
+    void getSingleDetectingVector(std::vector<float>& singleDetectorVector, std::vector<unsigned int>& singleDetectorVectorIndices) {
+        // Now we use the trained svm to retrieve the single detector vector
+        DOC** supveclist = model->supvec;
+        printf("Calculating single descriptor vector out of support vectors (may take some time)\n");
+        // Retrieve single detecting vector (v1 | b) from returned ones by calculating vec1 = sum_1_n (alpha_y*x_i). (vec1 is a n x1 column vector. n = feature vector length )
+        singleDetectorVector.clear();
+        singleDetectorVector.resize(model->totwords + 1, 0.);
+        printf("Resulting vector size %lu\n", singleDetectorVector.size());
+        
+        // Walk over every support vector
+        for (long ssv = 1; ssv < model->sv_num; ++ssv) { // Don't know what's inside model->supvec[0] ?!
+            // Get a single support vector
+            DOC* singleSupportVector = supveclist[ssv]; // Get next support vector
+            SVECTOR* singleSupportVectorValues = singleSupportVector->fvec;
+            WORD singleSupportVectorComponent;
+            // Walk through components of the support vector and populate our detector vector
+            for (unsigned long singleFeature = 0; singleFeature < model->totwords; ++singleFeature) {
+                singleSupportVectorComponent = singleSupportVectorValues->words[singleFeature];
+                singleDetectorVector.at(singleSupportVectorComponent.wnum) += (singleSupportVectorComponent.weight * model->alpha[ssv]);
+            }
+        }
+
+        // This is a threshold value which is also recorded in the lear code in lib/windetect.cpp at line 1297 as linearbias and in the original paper as constant epsilon, but no comment on how it is generated
+        singleDetectorVector.at(model->totwords) = -model->b; /** @NOTE the minus sign! */
+    }
+
+};
+
+/// Singleton
+SVMlight* SVMlight::getInstance() {
+    static SVMlight theInstance;
+    return &theInstance;
+}
+
+#endif	/* SVMLIGHT_H */
